6684ba66c87e049614e13d1bfb11807a2be5799d
Maks <pvl1720@gmail.com>
2017-04-22 11:11:11 -0700

    I think we added user and pswd right

--- starter-code/server.js
+++ starter-code/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password
 // const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://tupac:thuglife@localhost:5432';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
